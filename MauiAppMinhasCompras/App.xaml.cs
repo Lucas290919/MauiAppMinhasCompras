12b11b5db6f6b757bd8d3b8ff9172bfd2a19da2f
Lucas290919 <luveneroso19@gmail.com>
2025-08-07 17:17:41 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new Resources.Views.ListaProduto());
+        }
+    }
+}
